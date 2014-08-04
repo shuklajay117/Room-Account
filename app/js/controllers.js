486c0d6d65f727a52bca240f9a4783a477a07f91
unknown <shuklajay.117@gmail.com>
2014-08-04 00:17:28 -0700

    Login form integrated

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,9 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('LoginCtrl', ['$scope', function($scope) {
 
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+  .controller('RegisterCtrl', ['$scope', function($scope) {
 
   }]);
